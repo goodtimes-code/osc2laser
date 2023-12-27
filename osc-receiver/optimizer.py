d58146339b447eba9cd08a8e07e00df065f238cc
goodtimes-code <info@oliverbaltz.de>
2023-12-27 09:14:57 +0100

    Code works, just re-factored into sub functions with comments

--- osc-receiver/optimizer.py
+++ osc-receiver/optimizer.py
@@ -6,73 +6,129 @@ from copy import copy
 import global_data
 import time
 
+def apply_effects_to_laser_points(visible_laser_object):
+    """
+    Applies effects to all points in a visible laser object based on the effects defined in it.
+
+    Parameters:
+    - visible_laser_object: The laser object with points and effects.
+
+    Returns:
+    - A list of points with applied effects like position shift and RGB intensity.
+    """
+    optimized_points = []
+    rgb_intensity, x_shift, y_shift = 0, 0, 0
+
+    # Extracting effect levels from the visible laser object
+    for effect in visible_laser_object.effects:
+        if effect.name == 'RGB_INTENSITY':
+            rgb_intensity = effect.level
+        elif effect.name == 'X_POS':
+            x_shift = effect.level
+        elif effect.name == 'Y_POS':
+            y_shift = effect.level
+
+    # Apply the extracted effects to each point
+    for laser_point in visible_laser_object.point_list:
+        optimized_point = copy(laser_point)
+        optimized_point.x += x_shift
+        optimized_point.y += y_shift
+
+        for color in ['r', 'g', 'b']:
+            if rgb_intensity > 0:
+                setattr(optimized_point, color, getattr(optimized_point, color) / 255 * rgb_intensity)
+            else:
+                setattr(optimized_point, color, 0)
+        
+        optimized_points.append(optimized_point)
+
+    return optimized_points
+
+def interpolate_points(point1, point2, num_points):
+    """
+    Interpolates points between two given laser points.
+
+    Parameters:
+    - point1, point2: The start and end points for interpolation.
+    - num_points: The number of points to interpolate between the two points.
+
+    Returns:
+    - Arrays of interpolated x and y coordinates.
+    """
+    interpolated_x_coords = np.linspace(point1.x, point2.x, num=num_points)
+    interpolated_y_coords = np.linspace(point1.y, point2.y, num=num_points)
+    return interpolated_x_coords, interpolated_y_coords
+
+def adjust_points_for_screen(optimized_point_list, screen_width, screen_height, blank_point):
+    """
+    Adjusts points to ensure they fit within the screen dimensions. If a point falls outside the screen,
+    it is replaced with a blank point at the nearest border.
+
+    Parameters:
+    - optimized_point_list: List of optimized points.
+    - screen_width, screen_height: Dimensions of the screen.
+    - blank_point: A point with no color used to represent out-of-bounds points.
+
+    Returns:
+    - List of points adjusted for screen boundaries.
+    """
+    new_optimized_point_list = []
+    for pt in optimized_point_list:
+        if 0 <= pt.x <= screen_width and 0 <= pt.y <= screen_height:
+            new_optimized_point_list.append(pt)
+        else:
+            border_point = copy(blank_point)
+            border_point.x = max(0, min(pt.x, screen_width))
+            border_point.y = max(0, min(pt.y, screen_height))
+            new_optimized_point_list.append(border_point)
+    return new_optimized_point_list
+
 def get_optimized_point_list():
+    """
+    Generates an optimized list of points for laser output. This includes applying effects,
+    interpolating points, and adjusting for screen boundaries.
+
+    Parameters:
+    - global_data: Global data containing configuration and visible laser objects.
+
+    Returns:
+    - A list of optimized laser points.
+    """
     optimized_point_list = []
-    
     for visible_laser_object in global_data.visible_laser_objects:
         visible_laser_object.update()
 
-        rgb_intensity = 0
-        y_shift = 0
-        x_shift = 0
+        applied_points = apply_effects_to_laser_points(visible_laser_object)
 
-        for effect in visible_laser_object.effects:
-            if effect.name == 'RGB_INTENSITY':
-                rgb_intensity = effect.level
-            if effect.name == 'X_POS':
-                x_shift = effect.level
-            elif effect.name == 'Y_POS':
-                y_shift = effect.level
-
+        # Add initial blank points to avoid sudden jumps in the laser projection.
         blank_point_frames = int(global_data.config['laser_output']['blank_point_frames'])
-        blank_point = LaserPoint(visible_laser_object.point_list[0].x + x_shift, visible_laser_object.point_list[0].y + y_shift)
+        blank_point = LaserPoint(applied_points[0].x, applied_points[0].y)
         blank_point.set_color(0, 0, 0)
         optimized_point_list.extend([copy(blank_point)] * blank_point_frames)
 
         previously_optimized_laser_point = None
-        for laser_point in visible_laser_object.point_list:
-            optimized_laser_point = copy(laser_point)
-            optimized_laser_point.x += x_shift
-            optimized_laser_point.y += y_shift
-
-            for color in ['r', 'g', 'b']:
-                if rgb_intensity > 0:
-                    setattr(optimized_laser_point, color, getattr(optimized_laser_point, color) / 255 * rgb_intensity)
-                else:
-                    setattr(optimized_laser_point, color, 0)
-
+        for optimized_point in applied_points:
             if previously_optimized_laser_point and len(visible_laser_object.point_list) < int(global_data.config['laser_output']['interpolated_points']):
-                interpolated_x_coords = np.linspace(previously_optimized_laser_point.x, optimized_laser_point.x, num=int(global_data.config['laser_output']['interpolated_points']))
-                interpolated_y_coords = np.linspace(previously_optimized_laser_point.y, optimized_laser_point.y, num=int(global_data.config['laser_output']['interpolated_points']))
+                interpolated_x_coords, interpolated_y_coords = interpolate_points(previously_optimized_laser_point, optimized_point, int(global_data.config['laser_output']['interpolated_points']))
 
                 for x, y in zip(interpolated_x_coords, interpolated_y_coords):
-                    interpolated_point = copy(optimized_laser_point)
+                    interpolated_point = copy(optimized_point)
                     interpolated_point.x = x
                     interpolated_point.y = y
                     optimized_point_list.append(interpolated_point)
             else:
-                optimized_point_list.append(optimized_laser_point)
+                optimized_point_list.append(optimized_point)
 
-            previously_optimized_laser_point = optimized_laser_point
+            previously_optimized_laser_point = optimized_point
 
+        # Add a blank point at the end to ensure a smooth transition at the end of the sequence.
         blank_point_end = copy(blank_point)
         optimized_point_list.append(blank_point_end)
 
         screen_width = int(global_data.config['laser_output']['width'])
         screen_height = int(global_data.config['laser_output']['height'])
-        
-        # New logic for handling points out of screen
-        new_optimized_point_list = []
-        for pt in optimized_point_list:
-            if 0 <= pt.x <= screen_width and 0 <= pt.y <= screen_height:
-                new_optimized_point_list.append(pt)
-            else:
-                # Add a blank point near the border instead of removing the point
-                border_point = copy(blank_point)
-                border_point.x = max(0, min(pt.x, screen_width))
-                border_point.y = max(0, min(pt.y, screen_height))
-                new_optimized_point_list.append(border_point)
 
-        optimized_point_list = new_optimized_point_list
+        # Adjust points to fit within screen bounds, replacing out-of-bounds points with blank points.
+        optimized_point_list = adjust_points_for_screen(optimized_point_list, screen_width, screen_height, blank_point)
 
     return optimized_point_list
